8400fa3b7604b2da18677995e08556b1f47abbb4
Shuvo <74643721+AhmdShuvo@users.noreply.github.com>
2021-11-13 00:25:56 +0600

    Update Footer.js

--- cars-dekho/src/Components/Footer/Footer.js
+++ cars-dekho/src/Components/Footer/Footer.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <footer className="bg-dark mt-5 p-3">
+        <footer className="bg-dark mt-5 pt-3">
 
            
               <center><h3 className="text-light p-3">CopyRight &copy; cars dekhoo 2021</h3></center>
